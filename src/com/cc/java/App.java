f68cdceeb43df0e7bacf51b53c3d06a008b6924e
JGRO182 <j-p.grosse-kohlbrecher@web.de>
2021-12-02 14:34:26 +0100

    einfache Lösung
    Signed-off-by: JGRO182 <j-p.grosse-kohlbrecher@web.de>

--- src/com/cc/java/App.java
+++ src/com/cc/java/App.java
@@ -4,7 +4,23 @@ public class App {
     
     public static void main(String[] args) {
 
-    
+        Konto konto1 = new Konto();
+        Konto konto2 = new Konto();
+        Konto konto3 = new Konto();
+
+        System.out.println(konto1.kontostand);
+        System.out.println(konto2.kontostand);
+        System.out.println(konto3.kontostand);
+
+        konto1.kontostand = konto1.kontostand * 2;
+        konto2.kontostand = konto2.kontostand * 3;
+        konto3.kontostand = konto3.kontostand * 10;
+
+        System.out.println(konto1.kontostand);
+        System.out.println(konto2.kontostand);
+        System.out.println(konto3.kontostand);
+
+
     }
 
 }
